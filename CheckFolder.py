5a83687476ced418b086188e7614f65fffecc67d
Dmitriy <73654387+HannibalLecters@users.noreply.github.com>
2020-11-18 18:26:56 +0500

    Add files via upload

---
+++ CheckFolder.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+
+path_to_folder = ('C:\\WG\\WoT_trunks\\win64\\Reports\\')
+
+if len(os.listdir(path_to_folder)) == 0:
+    print('Объекты не найдены')
+
+for f in (os.listdir(path_to_folder)):
+    os.chdir(path_to_folder)
+    print('Объект найден -->', f, datetime.datetime.fromtimestamp(os.path.getctime(f)))
